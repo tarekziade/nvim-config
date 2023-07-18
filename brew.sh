fcaca40bdac92edd53623e50b8ecc886dd6955fd
Tarek ZIADE <tarek@ziade.org>
2023-07-18 15:51:32 +0200

    brew

---
+++ brew.sh
@@ -0,0 +1,8 @@
+brew install cloc colima docker docker-compose fd geckodriver \
+	gh git go hadolint htop jq lua-language-server lua@5.1 \
+	luarocks minikube neovim openjdk pkg-config poppler \
+	ripgrep sloc tectonic trash trivy wget zsh-completions
+
+brew tap homebrew/cask-fonts
+brew cask install font-hack-nerd-font
+
