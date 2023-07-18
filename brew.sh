7dc7a5bf1b61c5a050d8f6f8bb44fe5f4f6b99c4
Tarek ZIADE <tarek@ziade.org>
2023-07-18 16:00:44 +0200

    add rust

--- brew.sh
+++ brew.sh
@@ -1,8 +1,10 @@
 brew install cloc colima docker docker-compose fd geckodriver \
 	gh git go hadolint htop jq lua-language-server lua@5.1 \
-	luarocks minikube neovim openjdk pkg-config poppler \
+	luarocks minikube neovim pkg-config poppler \
 	ripgrep sloc tectonic trash trivy wget zsh-completions
 
 brew tap homebrew/cask-fonts
 brew cask install font-hack-nerd-font
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
