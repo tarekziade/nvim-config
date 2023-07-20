5bf9a2119d7bd6a558c5d4b5bb7ce336f13cc2be
Tarek ZIADE <tarek@ziade.org>
2023-07-20 07:57:33 +0200

    added more pkgs

--- install.sh
+++ install.sh
@@ -6,10 +6,13 @@ brew install cloc colima docker docker-compose fd geckodriver \
 	gh git go hadolint htop jq lua-language-server lua@5.1 \
 	luarocks minikube neovim pkg-config poppler \
 	ripgrep sloc tectonic trash trivy wget zsh-completions \
-	bash-language-server
+	bash-language-server tailwindcss-language-server \
+	typescript-language-server eslint node
 
 brew tap homebrew/cask-fonts
 brew cask install font-hack-nerd-font
+
+npm install -g eslint_d
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source "$HOME/.cargo/env"
